150f607869b620bfcf7fda5fe3aeb8262f67e7c5
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-05-07 16:31:23 -0400

    checkbox and views updated

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -25,7 +25,24 @@ class QuestionsController < ApplicationController
 
 
   def edit
+    @question = Question.find(params[:id])
   end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.title = params[:question][:title]
+    @question.body = params[:question][:body]
+    @question.resolved = params[:question][:resolved]
+
+    if @question.save
+      flash[:notice] = "Question was updated."
+      redirect_to @question
+    else
+      flash.now[:alert] = "There was an error saving the question. Please try again."
+      render :edit
+    end
+  end
+
 
   def show
     @question = Question.find(params[:id])
