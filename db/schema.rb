1f44035a374f57c5572d520101e083c4e4a891db
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-05-09 15:08:38 -0400

    topic model and spec created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180506224719) do
+ActiveRecord::Schema.define(version: 20180509190216) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 20180506224719) do
     t.string "title"
     t.text "body"
     t.boolean "resolved"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name"
+    t.boolean "public", default: true
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
