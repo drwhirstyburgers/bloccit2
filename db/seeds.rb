9de4ef7ba091779d78926af88cb36e50ae2e3c21
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-05-09 15:40:18 -0400

    spec for topics/posts/comments updated, seed updated, and migration and keys created for posts/topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
 require 'random_data'
 
+15.times do
+  Topic.create!(
+    name: RandomData.random_sentence,
+    description: RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
 50.times do
   Post.create!(
+    topic: topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
@@ -20,5 +29,6 @@ unique_post = Post.find_or_create_by(title: "this is a unique post", body: "this
 Comment.find_or_create_by(post: unique_post, body: "This is a unique comment body")
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
