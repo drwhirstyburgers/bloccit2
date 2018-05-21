6e63f31208bc43ed27182ed41b76ce3e9f6e999a
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-05-20 20:42:14 -0400

    code written for create_vote and spec passed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  after_create :create_vote
+
   belongs_to :topic
   belongs_to :user
   has_many :comments, dependent: :destroy
@@ -27,5 +29,11 @@ class Post < ApplicationRecord
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
   end
 end
