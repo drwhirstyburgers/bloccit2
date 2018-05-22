aca00989c08a2b63d5635d640b326dc40f62271d
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-05-22 16:01:40 -0400

    helper method for user view posts/comments created

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
 module UsersHelper
+  def is_posts(user)
+    if user.posts.count == 0
+      "#{user.name} has not submitted any posts yet."
+    else
+      render @user.posts
+    end
+  end
+
+  def is_comments(user)
+    if user.comments.count == 0
+      "#{user.name} has not submitted any comments yet."
+    else
+      render @user.comments
+    end
+  end
 end
