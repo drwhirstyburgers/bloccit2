7f153fb918d159c96bc63ae701e2a86f17e8c6c9
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-05-22 15:10:17 -0400

    spec refactored for factories

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -98,8 +98,8 @@ RSpec.describe User, type: :model do
   end
 
   describe "invalid user" do
-     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
-     let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
+    let(:user_with_invalid_name) { build(:user, name: "") }
+    let(:user_with_invalid_email) { build(:user, email: "") }
 
      it "should be an invalid user due to blank name" do
        expect(user_with_invalid_name).to_not be_valid
@@ -128,7 +128,7 @@ RSpec.describe User, type: :model do
 
    describe ".avatar_url" do
      let(:known_user) { create(:user, email: "blochead@bloc.io") }
- 
+
      it "returns the proper Gravatar url for a known email entity" do
        expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
        expect(known_user.avatar_url(48)).to eq(expected_gravatar)
